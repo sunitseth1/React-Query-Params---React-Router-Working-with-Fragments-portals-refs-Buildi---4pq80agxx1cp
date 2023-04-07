804899c703c42d1a29246ee00c4fe6705c7a61e7
sunit seth <100702483+sunitseth1@users.noreply.github.com>
2023-04-07 16:57:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import '../styles/App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
-const App = () => {
-  //Write your code here
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
-
 export default App;
